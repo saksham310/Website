3bc5997534e042f7a5fd83fd9c78a0cdd3f52591
saksham sharma <sakshams982@gmail.com>
2025-07-21 15:57:05 +0545

    - Updated the contact page to include socials icon

--- src/app/connect/connect.ts
+++ src/app/connect/connect.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {LucideAngularModule, Github, LinkedinIcon,Mail} from 'lucide-angular';
 
 @Component({
   selector: 'app-connect',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './connect.html',
   styleUrl: './connect.css'
 })
 export class Connect {
-
+  protected readonly icon = {
+    'mail':Mail,
+    'linkedin':LinkedinIcon,
+    'github':Github
+  }
 }
