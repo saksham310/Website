7000a6d4ac4f6d5805e62b4955d5b4e5354c0397
saksham sharma <sakshams982@gmail.com>
2025-07-21 19:35:16 +0545

    - Updated the experience section to include the work experience

--- src/app/experience/experience.ts
+++ src/app/experience/experience.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './experience.css'
 })
 export class Experience {
+  protected readonly  achievements = [
+    'Architected a reusable component library in Angular that increased development velocity by 40%.',
+    'Optimized application performance using advanced data structures (Set, Map) to enhance user experience.',
+    'Translated complex Figma designs into pixel-perfect, responsive, and accessible user interfaces.'
+  ];
 
 }
